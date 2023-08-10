50d071490a661d0bc50653567a737183cdaf0f85
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:29:58 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-releases-toolbox";
+metadata.url = "https://github.com/test-testperson/sample-releases-toolbox";
+end
